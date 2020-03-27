fd0eac8f49c8aecfd73f5a15eaed5a0c97901c69
cotneok <tokrostsvaridze@vaba.co>
2020-03-27 14:42:06 +0400

    Add koala gem for facebook api

---
+++ config/initializers/koala.rb
@@ -0,0 +1,6 @@
+Koala.configure do |config|
+  config.app_id = "562588991304187"
+  config.app_secret = "562fc92676427661f6fb152efade30bc"
+  # See Koala::Configuration for more options, including details on how to send requests through
+  # your own proxy servers.
+end
