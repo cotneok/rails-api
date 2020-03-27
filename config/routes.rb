d96ea5a62453224131edf4fcd7102be6bf429bdc
cotneok <tokrostsvaridze@vaba.co>
2020-03-27 13:19:16 +0400

    Add sign in function and route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       devise_scope :user do
         post "sign_up", to: "registrations#create"
-      end 
+        post "sign_in", to: "sessions#create"
+      end
     end
   end
 end
